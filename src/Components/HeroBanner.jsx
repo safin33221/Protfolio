b912efe0341eacf493aa23f657f69caf3d507396
safin33221 <saifn33221@gmail.com>
2025-01-04 00:13:36 +0600

    competed to implement social profile link in banner

--- src/Components/HeroBanner.jsx
+++ src/Components/HeroBanner.jsx
@@ -1,12 +1,13 @@
 import profile from '../assets/safin.png'
 import resume from '../assets/safayet_hossan.pdf'
+import { FaFacebook, FaGithub, FaLinkedin, FaLinkedinIn, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 
 const HeroBanner = () => {
     return (
         <div className="hero bg-[#251C31] text-white min-h-screen mt-4">
             <div className="hero-content  flex-col lg:flex-row-reverse">
-                <div   className='w-1/2 mx-auto  '>
-                    
+                <div className='w-1/2 mx-auto  '>
+
                     <img
                         src={profile}
                         className="max-w-sm rounded-full " />
@@ -18,8 +19,18 @@ const HeroBanner = () => {
                         I Make beautiful landing page
                     </p>
                     <a href={resume} download={resume} className="p-3 rounded-lg bg-[#693B93]  border-0 border-[#542992]   border-b-4   ">My Resume</a>
+
+                    <div className=' mt-10'>
+                        <ul className='flex gap-4'>
+                            <li className='text-4xl text-[#1877F2]'><a href="https://web.facebook.com/safin.2360/" target='_blank'><FaFacebook /></a></li>
+                            <li className='text-4xl text-[#7b7777]'><a href="https://github.com/safin33221" target='_blank'><FaGithub /></a></li>
+                            <li className='text-4xl text-[#25D366]'><a href="https://wa.me/+8801837429636/" target='_blank'><FaWhatsapp /></a></li>
+                            <li className='text-4xl text-[#0072B1]'><a href="https://www.linkedin.com/in/safayet-hossan-safin/" target='_blank'><FaLinkedinIn /></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
